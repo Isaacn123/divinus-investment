99cae9b79c08e8ddac2762def54aef4d598c1ab2
Isaacn123 <nsambai72@gmail.com>
2026-01-12 23:17:38 +0300

    changing the image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,6 +3,7 @@
 import { Button } from './ui/button';
 import { motion } from 'motion/react';
 import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
 
 export function Hero() {
   return (
@@ -74,7 +75,7 @@ export function Hero() {
           >
             <div className="relative rounded-2xl overflow-hidden shadow-2xl">
               <img 
-                src="https://images.unsplash.com/photo-1734255026082-82fdc81991f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxBZnJpY2FuJTIwYnVzaW5lc3MlMjBncm93dGh8ZW58MXx8fHwxNzY3ODEyNzI5fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+                src="/Digital_Investment.png"
                 alt="African business growth"
                 className="w-full h-auto"
               />
