3d45665d84cb47c8b1476142809671bb329dda7f
Isaacn123 <nsambai72@gmail.com>
2026-01-12 23:29:32 +0300

    changing the image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -75,7 +75,8 @@ export function Hero() {
           >
             <div className="relative rounded-2xl overflow-hidden shadow-2xl">
               <img 
-                src="/Digital_Investment.png"
+                // src="/Digital_Investment.png"
+                src="https://plus.unsplash.com/premium_photo-1663045775483-488b915b5789?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
                 alt="African business growth"
                 className="w-full h-auto"
               />
